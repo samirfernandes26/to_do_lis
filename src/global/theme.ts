5adb71225eb7f11f6033a0314728dae9096ae440
Samir Fernandes de Lima Resende <samirfernandes@gmail.com>
2023-05-15 17:40:28 -0300

    .

---
+++ src/global/theme.ts
@@ -0,0 +1,21 @@
+export default {
+  colors: {
+    purple: '#8284FA',
+    purple_dark: '#5E60CE',
+    blue: '#4EA8DE',
+    blue_dark: '#1E6F9F',
+    gray: '#0D0D0D',
+    gray_600: '#1A1A1A',
+    gray_500: '#262626',
+    gray_400: '#333333',
+    gray_300: '#808080',
+    gray_200: '#D9D9D9',
+    gray_100: '#F2F2F2',
+    danger: '#E25858',
+  },
+
+  fonts: {
+    regular: 'Inter-Regular',
+    black: 'Inter-Black',
+  },
+};
